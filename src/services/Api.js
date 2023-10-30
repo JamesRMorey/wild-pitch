d3e2d738f38a6e660291992d3b02d2d189f208d5
James Morey <jamesrmorey@gmail.com>
2023-10-30 20:46:26 +0000

    test - set these up as env

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 axios.defaults.withCredentials = true;
-axios.defaults.baseURL = 'http://localhost:8000/';
+axios.defaults.baseURL = 'http://api.wild-pitch.co.uk/';
 
 export default class Api {
     constructor() {}
