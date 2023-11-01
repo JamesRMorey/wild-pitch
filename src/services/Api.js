c2e4d9cdc86fd2ea4a08db1625bb67f1efa0b363
James Morey <jamesrmorey@gmail.com>
2023-11-01 13:03:51 +0000

    test

--- src/services/Api.js
+++ src/services/Api.js
@@ -20,14 +20,16 @@ export default class Api {
     
     async login( email, password ) {
         return new Promise(async ( resolve, reject ) => {
-            await axios.get( 'sanctum/csrf-cookie' )
-            await axios.post( 'login', { email: email, password: password } )
-            .then(( response ) => {
-                resolve( response.data );
+            await axios.get( 'sanctum/csrf-cookie' ).then(( cookie ) => {
+                await axios.post( 'login', { email: email, password: password } )
+                .then(( response ) => {
+                    resolve( response.data );
+                })
+                .catch(( error ) => {
+                    reject( error.response.data );
+                })
             })
-            .catch(( error ) => {
-                reject( error.response.data );
-            })
+            
         })
     }
 
