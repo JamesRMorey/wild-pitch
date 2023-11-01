53085c726898b86d220ef20f6b6b5ad92768a7e0
James Morey <jamesrmorey@gmail.com>
2023-11-01 19:56:17 +0000

    backgrounds

--- src/services/Consts.js
+++ src/services/Consts.js
@@ -3,6 +3,4 @@ export const backgrounds = [
     '/backgrounds/background_2.webp',
     '/backgrounds/background_3.webp',
     '/backgrounds/background_4.webp',
-    '/backgrounds/background_5.webp',
-    '/backgrounds/background_6.webp',
 ];
