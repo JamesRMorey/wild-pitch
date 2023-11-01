19838fa886e5fd4d5986baad8dac377a144522ec
James Morey <jamesrmorey@gmail.com>
2023-11-01 19:25:23 +0000

    filters

--- src/views/pitch/Listing.vue
+++ src/views/pitch/Listing.vue
@@ -155,10 +155,10 @@ onMounted(async () => {
                     <IconButton @press="handleMapModeClick" :active="map.show" icon="fa-map"/>
                 </div>
             </div>
-            <div v-if="config.loaded" class="inline-flex pt-8 gap-8 z-1 flex-col lg:flex-row items-center lg:items-start">
-                <div v-if="filters.show" class="w-64 flex-col inline-flex">
-                    <div class="w-full inline-flex flex-col shadow p-5 rounded-xl">
-                        <div class="text-lg font-semibold">Search Radius</div>
+            <div v-if="config.loaded" class="inline-flex pt-8 gap-8 z-1 flex-col lg:flex-col items-center lg:items-start">
+                <div v-if="filters.show" class="w-64 flex-col inline-flex items-start bg-gray-100 shadow p-5 rounded-xl w-full">
+                    <div class="p-5 rounded-xl w-full md:w-1/2 lg:w-1/3 bg-white">
+                        <div class="text-lg font">Radius</div>
                         <select
                             id="select"
                             v-model="filters.radius"
